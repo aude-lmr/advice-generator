cbc3c11da4bc7edf9edc9cf2f925490e3b60374c
Aude Limeri <limeri.aude@gmail.com>
2024-08-31 00:42:32 -0400

    Adding box-shadow on the button - correcting code error

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import DecorativeLines from "../app/components/decorativeligns";
+// import DecorativeLines from "../app/components/decorativeligns";
 
 export default function Home() {
   const [advice, setAdvice] = useState({});
