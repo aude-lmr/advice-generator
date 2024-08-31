6de3ab718bd06282defdc1507c7b6485b04e89f0
Aude Limeri <limeri.aude@gmail.com>
2024-08-31 00:39:17 -0400

    Add

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import DecorativeLines from "../app/components/decorativeligns";
 
 export default function Home() {
   const [advice, setAdvice] = useState({});
@@ -16,7 +17,7 @@ export default function Home() {
 
   return (
     <main className="flex justify-center items-center bg-dark-blue min-h-screen p-4">
-      <div className="box-border relative p-6 flex flex-col items-center justify-center w-full max-w-xs sm:max-w-md border border-solid border-grayish-blue bg-grayish-blue rounded-lg">
+      <div className="box-border relative p-6 flex flex-col items-center justify-center w-full max-w-xs sm:max-w-md min-h-min h-2/5 border border-solid border-grayish-blue bg-grayish-blue rounded-lg">
         <h1 className="text-xs sm:text-sm font-bold text-neon-green text-center">
           ADVICE #{advice.id}
         </h1>
@@ -24,8 +25,10 @@ export default function Home() {
           {advice.advice}
         </p>
 
+        {/* <DecorativeLines /> */}
+
         <button
-          className="flex justify-center items-center absolute -bottom-6 sm:-bottom-7 border-solid border-neon-green bg-neon-green rounded-full w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16"
+          className="flex justify-center items-center absolute -bottom-6 sm:-bottom-7 border-solid border-neon-green bg-neon-green rounded-full w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 z-10 hover:shadow-2xl  hover:shadow-neon-green active:shadow-2xl active:shadow-neon-green"
           onClick={fetchAdvice}
         >
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
